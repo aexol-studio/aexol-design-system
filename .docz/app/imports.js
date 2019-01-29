183233598e68d0cd8e7f6450690059e9d4c72d5e
Ania Łysiuk <anna.lysiuk86@gmail.com>
2019-01-29 10:24:16 +0100

    delete ignored

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
-  'components/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "components-button" */ 'components/Button.mdx'),
-}
