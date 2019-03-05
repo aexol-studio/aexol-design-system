8464d93e5a9d08e3c690bee59ffa993a52e72f93
aexol <aexol@aexol.com>
2019-03-05 15:03:54 +0100

    fix(typography): corrected line height

--- src/components/typography/style.ts
+++ src/components/typography/style.ts
@@ -4,38 +4,38 @@ export const BaseTypoGraphy: types.NestedCSSProperties = {
   fontStyle: 'normal'
 };
 export const BaseTypoGraphyP: types.NestedCSSProperties = {
-  lineHeight: 23,
+  lineHeight: `23px`,
   fontSize: 16
 };
 
 export const TypoGraphy = {
   H0: style({
     ...BaseTypoGraphy,
-    lineHeight: 73,
+    lineHeight: `73px`,
     fontSize: 66,
     fontWeight: 'bold'
   }),
   H1: style({
     ...BaseTypoGraphy,
-    lineHeight: 48,
+    lineHeight: `48px`,
     fontSize: 50,
     fontWeight: 'bold'
   }),
   H2: style({
     ...BaseTypoGraphy,
-    lineHeight: 42,
+    lineHeight: `42px`,
     fontSize: 37,
     fontWeight: 'bold'
   }),
   H3: style({
     ...BaseTypoGraphy,
-    lineHeight: 28,
+    lineHeight: `28px`,
     fontSize: 28,
     fontWeight: 'bold'
   }),
   H4: style({
     ...BaseTypoGraphy,
-    lineHeight: 21,
+    lineHeight: `21px`,
     fontSize: 21,
     fontWeight: 500
   }),
@@ -56,7 +56,7 @@ export const TypoGraphy = {
   }),
   Label: style({
     ...BaseTypoGraphy,
-    lineHeight: 16,
+    lineHeight: `16px`,
     fontSize: 12,
     fontWeight: 500
   })
