c940bb3469f443bbc511a49dfa784a0e5abcfd44
krzysiek <krzysiek@krzysieks-Mac-mini.local>
2019-03-22 13:09:51 +0100

    blueTileStyle

---
+++ src/components/BlueTile.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react'
+import * as classnames from 'classnames';
+import { PFooterTitle} from './typography';
+import * as styles from './styles/FooterStyle';
+type FooterColumnsTxt = {
+  objects: Array<{
+    title?: string;
+    objects: Array<{
+      name: string;
+      link: string;
+    }>;
+  }>;
+}
+
+export interface FooterComponentProps {
+  FooterColumnsTxt: FooterColumnsTxt;
+  backgroundColor?: 'black' | 'white' ;
+  copyright: string;
+}
+
+export const Footer: React.FunctionComponent<FooterComponentProps> = (props) => {
+
+  const {
+    FooterColumnsTxt,
+    // logo,
+    backgroundColor= 'black',
+    copyright,
+    ...restProps
+  } = props
+
+  return (
+    <div
+      className={classnames(
+      styles.FooterMain,
+      backgroundColor)}
+    >
+      <div
+        className={
+          styles.FooterContainer
+        }
+        {...restProps}
+      >
+        <div className={styles.rectangleTopDiv}>
+          {/* <p className={styles.rectangleTopLogo}>
+            {logo}</p> */}
+          <div className={styles.rectangleTop} />
+        </div>
+        <div className={styles.FooterContent}>
+          {FooterColumnsTxt.objects.map((el, idxt) => {
+            return <div className={styles.FooterContentColumn} key={idxt}>
+              <div><PFooterTitle >{el.title}</PFooterTitle></div>
+              <div className={styles.PFooterNameDiv}>{el.objects.map((o, idx) =>
+                <a className={styles.PFooterName} href={o.link} key={idx}>{o.name}</a>)}</div>
+            </div>
+          })
+          }
+        </div>
+        <div className={styles.rectangleDownDiv}>
+          <div className={styles.rectangleDown} />
+          <p className={styles.rectangleDownCopyright}>
+            {copyright}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/styles/BlueTileStyles.tsx
@@ -0,0 +1,132 @@
+import { style, media } from 'typestyle';
+import { Colors } from './Colors';
+import {Breakpoints} from './Breakpoints';
+
+export const FooterContainer = style({
+  $debugName: 'FooterContainer',
+  width: '80%',
+  maxWidth: 1170,
+  margin: '0 auto',
+  marginTop: '80px',
+  marginBottom: '80px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexDirection: 'column'
+})
+
+export const rectangleTopDiv = style({
+  $debugName: 'rectangleTopDiv',
+  display: 'flex',
+  flexDirection: 'column'
+  // justifyContent: 'space-evenly',
+  // marginTop: 50
+})
+export const rectangleDownDiv = style({
+  $debugName: 'rectangleDownDiv',
+  display: 'flex',
+  // marginBottom: 50,
+  flexDirection: 'column'
+})
+
+export const rectangleTop = style({
+  $debugName: 'rectangleTop',
+  height: 3,
+  background: Colors['Ancient Stone']
+})
+
+export const rectangleDown = style({
+  $debugName: 'rectangleDown',
+  height: 3,
+  background: Colors['Ancient Stone']
+})
+
+export const rectangleTopLogo = style({
+  $debugName: 'rectangleTopLogo',
+  color: Colors['Ancient Stone'],
+  textAlign: 'center',
+  marginTop: 5,
+  marginBottom: 5,
+  fontSize: 30
+})
+
+export const rectangleDownCopyright = style({
+  $debugName: 'rectangleDownCopyright',
+  textAlign: 'left',
+  color: Colors['Ancient Stone'],
+  marginTop: 5,
+  marginBottom: 0,
+  fontSize: 12
+})
+
+export const FooterMain = style({
+  $debugName: 'FooterMain',
+  backgroundColor: Colors['Black Hole'],
+  display: 'flex',
+  justifyContent: 'space-around',
+  $nest: {
+    '&.white': {
+      backgroundColor: Colors.White
+    }
+  }
+})
+
+export const FooterContent = style({
+  $debugName: 'FooterContent',
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexWrap: 'wrap'
+  // marginLeft: '110px',
+  // marginRight: '110px'
+},
+  media({ minWidth: 0, maxWidth: Breakpoints.Tablet},
+    {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    })
+)
+
+export const FooterContentColumn = style({
+  $debugName: 'FooterContentColumn',
+  marginTop: 60,
+  marginBottom: 60,
+  flexDirection: 'column'
+},
+media({ minWidth: 0, maxWidth: 1200 },
+  {
+    marginTop: 35,
+    marginBottom: 35
+  })
+)
+
+export const FooterContentColumnTitle = style({
+  $debugName: 'FooterContentColumnTitle',
+  color: '#514E5A',
+  fontSize: 16,
+  fontWeight: 'bold',
+  marginBottom: 0,
+  marginTop: 0
+})
+export const FooterContentColumnName = style({
+  $debugName: 'FooterContentColumnTitle',
+  color: '#514E5A',
+  fontSize: 16,
+  marginBottom: 0,
+  marginTop: 5
+})
+
+export const PFooterNameDiv = style ({
+display: 'flex',
+flexDirection: 'column'
+})
+
+export const PFooterName = style ({
+    margin: 0,
+    padding: 0,
+    lineHeight: `23px`,
+    fontSize: 16,
+    color: '#514E5A',
+    marginBottom: 0,
+    marginTop: 5,
+    textDecoration: 'none'
+})
