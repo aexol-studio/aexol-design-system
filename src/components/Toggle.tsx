c5017ab01f3207279d16341b63ccdea7892de408
aexol <aexol@aexol.com>
2019-04-03 15:41:23 +0200

    feat(typeofcolors):

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import * as classnames from 'classnames';
 import { Close, Check } from './icons';
 import * as styles from './styles/ToggleStyle';
-import { Colors } from './styles/Colors';
 
-type ToggleShape = 'round' | 'oval'
+type ToggleShape = 'round' | 'oval';
 
 export interface IToggleProps {
   shape?: ToggleShape;
@@ -18,62 +17,57 @@ interface IToggleState {
 
 export class Toggle extends React.PureComponent<IToggleProps, IToggleState> {
   constructor(props: IToggleProps) {
-    super(props)
+    super(props);
     this.state = {
       value: props.value || false
-    }
+    };
   }
 
   handleOnClick = (value: boolean) => {
     if (this.state.value !== value) {
       this.setState({
         value: value
-      })
+      });
       if (this.props.onChange) {
-        this.props.onChange(value)
+        this.props.onChange(value);
       }
     }
-  }
+  };
 
   renderElements = () => {
-
-    return(
+    return (
       <React.Fragment>
         <div
-          className={classnames(classnames({
-            [styles.ToggleElement]: true,
-            active: this.state.value,
-            on: true
-          }),
-          classnames(
-            this.props.shape
-          ))}
+          className={classnames(
+            classnames({
+              [styles.ToggleElement]: true,
+              active: this.state.value,
+              on: true
+            }),
+            classnames(this.props.shape)
+          )}
           onClick={e => this.handleOnClick(true)}
         >
-          <Check fill={Colors.White} />
+          <Check fill={'White'} />
         </div>
         <div
-          className={classnames(classnames({
-            [styles.ToggleElement]: true,
-            active: !this.state.value,
-            off: true
-          }),
-          classnames(
-            this.props.shape
-          ))}
+          className={classnames(
+            classnames({
+              [styles.ToggleElement]: true,
+              active: !this.state.value,
+              off: true
+            }),
+            classnames(this.props.shape)
+          )}
           onClick={e => this.handleOnClick(false)}
         >
-          <Close fill={Colors.White}/>
+          <Close fill={'White'} />
         </div>
       </React.Fragment>
-    )
-  }
+    );
+  };
 
   render() {
-    return (
-      <div className={styles.Toggle}>
-        {this.renderElements()}
-      </div>
-    )
+    return <div className={styles.Toggle}>{this.renderElements()}</div>;
   }
 }
