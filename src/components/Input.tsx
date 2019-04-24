ee0e4fc53e9b7da8503b1ebdd239b29c2fe990c6
krzysiek bialy <krzysiek@aexol.com>
2019-04-24 08:32:17 +0200

    fix passwordText

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,7 +12,7 @@ export interface IInputProps {
   fullWidth?: boolean;
   style?: React.CSSProperties;
   password?: boolean;
-  passwordText: string;
+  passwordText?: string;
   passwordLink?: string;
 }
 
